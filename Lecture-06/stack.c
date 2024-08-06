283880d5a43d3698daea93b67f758f0d6d8bcbfe
saudz <saud.zahir@soco-engineers.com>
2024-08-06 10:08:43 +0500

    Initial commit

---
+++ Lecture-06/stack.c
@@ -0,0 +1,30 @@
+#include "stack.h"
+
+void stackNew(stack *s) {
+    s->logicallen = 0;
+    s->alloclen = 4;
+    s->elements = malloc(s->alloclen * sizeof(int));
+
+    assert(s->elements != NULL);
+}
+
+void stackDispose(stack *s) {
+    free(s->elements);
+}
+
+void stackPush(stack *s, int value) {
+    if (s->logicallen == s->alloclen) {
+        s->alloclen *= 2;
+        s->elements = realloc(s->elements, s->alloclen * sizeof(int));
+
+        assert(s->elements != NULL);
+    }
+
+    s->elements[s->logicallen] = value;
+    s->logicallen++;
+}
+
+int stackPop(stack *s) {
+    s->logicallen--;
+    return s->elements[s->logicallen];
+}
---
+++ Lecture-06/stack.h
@@ -0,0 +1,20 @@
+#pragma once;
+
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct {
+    int *elements;
+    int logicallen;
+    int alloclen;
+} stack;
+
+// Constructor
+void stackNew(stack *s);
+
+// Destructor
+void stackDispose(stack *s);
+
+// Methods
+void stackPush(stack *s, int value);
+int stackPop(stack *s);
